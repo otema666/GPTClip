c4beadb22285d2229e791acd4c613df3b5d8f9ea
otema <otema000@gmail.com>
2024-10-08 01:49:43 +0200

    Añadir prompts en español y archivo de cabecera
    Se ha añadido la inclusión del archivo de cabecera `prompts.h` en `prompts.c`.
    Se han definido dos nuevas constantes de tipo `const char*` en `prompts.c`:
      * `prompt_test`: Mensaje para analizar un problema tipo test.
      * `prompt_LongAnswer`: Mensaje para escribir una respuesta detallada.
    Se ha creado el archivo de cabecera `prompts.h` con:
      * Guardia de inclusión (`#ifndef PROMPTS` y `#define PROMPTS`).
      * Declaración `extern` de las constantes `prompt_test` y `prompt_LongAnswer`.
      * Finalización del guardia de inclusión con `#endif // PROMPTS`.

---
+++ GPTClip/prompts/prompts.c
@@ -0,0 +1,5 @@
+#include "prompts.h"
+
+
+const char* prompt_test = "Analiza el siguiente problema tipo test y determina cuál es la respuesta correcta.Recuerda que solo puedes responder con la letra correspondiente a la respuesta correcta.Si las respuestas no están enumeradas, identifica las diferentes opciones y asígnales letras(A, B, C, D, etc.) según tu criterio.\nProblema:\n";
+const char* prompt_LongAnswer = "Analiza el siguiente problema y escribe una respuesta detallada y completa. Recuerda que la respuesta debe ser coherente y estar bien redactada.\nProblema:\n";
---
+++ GPTClip/prompts/prompts.h
@@ -0,0 +1,7 @@
+#ifndef PROMPTS
+#define PROMPTS
+
+extern const char* prompt_test;
+extern const char* prompt_LongAnswer;
+
+#endif // PROMPTS
