06829e7e26ad29e06f80ee65aea019fd196baf14
otema <otema000@gmail.com>
2024-10-13 02:30:52 +0200

    Renombrar parámetro y añadir cabecera HTTP
    Se ha cambiado el parámetro `pregunta` por `question` en las verificaciones de `$_GET` y en la asignación de variables.
    Se ha añadido una cabecera HTTP para especificar el tipo de contenido como texto plano con codificación UTF-8 antes de imprimir el resultado.

--- GPTClip/api/gptApi.php
+++ GPTClip/api/gptApi.php
@@ -1,11 +1,12 @@
 <?php
-if (isset($_GET['prompt']) && isset($_GET['pregunta'])) {
+if (isset($_GET['prompt']) && isset($_GET['question'])) {
     $prompt = escapeshellarg($_GET['prompt']);
-    $pregunta = escapeshellarg($_GET['pregunta']);
+    $pregunta = escapeshellarg($_GET['question']);
 
     $command = "/var/www/html/gptClip/.venv/bin/python /var/www/html/gptClip/duckduckgo-chat-ai/gpt.py $prompt $pregunta";
     
     $output = shell_exec($command);
+    header('Content-Type: text/plain; charset=utf-8');
 
     echo "$output";
 }
