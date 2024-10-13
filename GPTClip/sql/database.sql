29e52c2cac615845013842fdafbcc327464479a4
otema <otema000@gmail.com>
2024-10-13 15:20:04 +0200

    Refactorización de la tabla usuarios y creación de DB
    Se ha añadido un script para la creación de una base de datos MySQL llamada `gptClip` y se ha especificado su uso con `USE gptClip`.
    
    La tabla `usuarios` ha sido eliminada y reemplazada con una nueva definición que incluye las siguientes modificaciones:
    - `nombre_usuario` renombrado a `username` y ahora es único (`UNIQUE`).
    - `contrasena` renombrado a `password`.
    - Nuevo campo `is_logged` de tipo `tinyint(1)` con valor por defecto `0`.
    - `fecha_creacion` renombrado a `creation_date`, ahora puede ser `NULL` y su valor por defecto sigue siendo `CURRENT_TIMESTAMP`.
    - Especificado el motor de almacenamiento `InnoDB` y el conjunto de caracteres `utf8mb4` con la colación `utf8mb4_0900_ai_ci`.

--- GPTClip/sql/database.sql
+++ GPTClip/sql/database.sql
@@ -1,8 +1,12 @@
+-- MySQL database creation script
 CREATE DATABASE gptClip;
 USE gptClip;
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,    -- PK
-    nombre_usuario VARCHAR(50) NOT NULL,  -- Nombre de usuario
-    contrasena VARCHAR(255) NOT NULL,     -- Contraseña
-    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- Fecha de creación del usuario
-);
+
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,                                   -- PK
+  `username` varchar(50) NOT NULL UNIQUE,                             -- Nombre de usuario
+  `password` varchar(255) NOT NULL,                                   -- Contraseña
+  `is_logged` tinyint(1) NOT NULL DEFAULT 0,                          -- ¿Está logueado?
+  `creation_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,           -- Fecha de creación
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;   -- Unicode
